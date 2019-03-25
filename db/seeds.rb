503c808750b44ca821a4e7283edf1d8a0758e2fb
Jimmy Le Rouzic <jimmy.lerouzic@gmail.com>
2019-03-25 18:57:57 +0100

    finished seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,177 +5,100 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 puts "clean up DB"
 
 Bin.destroy_all
 User.destroy_all
 
 puts "Start database creation... "
-
-# USERS
   poubelle_papier1 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/bin1.jpg')),
   kind: 'papier',
   name: 'mat',
-  longitude: 47.2185254,
-  latitude: -1.5671195
+  address: '14 Rue Beauregard, 44000 Nantes',
+  longitude: 47.2134354,
+  latitude: -1.5556044
 )
 
   poubelle_papier2 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/bin2.jpg')),
   kind: 'papier',
   name: 'jor',
-  longitude: 47.2187063,
-  latitude: -1.5685133
+  address: '26 Rue Stanislas Baudry, 44400 Nantes',
+  longitude: 47.2181645,
+  latitude: -1.5430119
 )
   poubelle_papier3 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/bin3.jpg')),
   kind: 'papier',
   name: 'sim',
-  longitude: 47.2186982,
-  latitude: -1.5689721
+  address: '7 Impasse Saint-Laurent, 44000 Nantes',
+  longitude: 47.2183730,
+  latitude: -1.5501451
 )
   poubelle_papier4 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/bin4.jpg')),
   kind: 'papier',
   name: 'jim',
-  longitude: 47.218705,
-  latitude: -1.5690397
+  address: '6 Place Aristide Briand, 44003 Nantes',
+  longitude: 47.2177317,
+  latitude: -1.5633201
 )
   poubelle_papier5 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/bin5.jpg')),
   kind: 'papier',
   name: 'jem',
-  longitude: 47.2183323,
-  latitude: -1.5620625
+  address: '13 Rue de la Juiverie, 44000 Nantes',
+  longitude: 47.2154289,
+  latitude: -1.5530633
 )
   poubelle_menagere1 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/bin6.jpg')),
   kind: 'menager',
   name: 'john',
-  longitude: 47.2185081,
-  latitude: -1.5545757
+  address: 'Rue de Talensac, 44009 Nantes',
+  longitude: 47.2212440,
+  latitude: -1.5579771
 )
 poubelle_menagere2 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/bin7.jpg')),
   kind: 'menager',
   name: 'jacques',
-  longitude: 47.2140721,
-  latitude: -1.5576681
+  address: '12 Rue Paul Bellamy, 44000 Nantes',
+  longitude: 47.2213460,
+  latitude: -1.5557241
 )
 poubelle_menagere3 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/bin8.jpg')),
   kind: 'menager',
   name: 'josh',
-  longitude: 47.2124104,
-  latitude: -1.5556372
+  address: 'Rue Sully, 44000 Nantes',
+  longitude: 47.2206028,
+  latitude: -1.5507245
 )
 poubelle_menagere4 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/bin9.jpg')),
   kind: 'menager',
   name: 'bruno',
-  longitude: 47.2124234,
-  latitude: -1.5556044
+  address: '31 Place Viarme, 44000 Nantes',
+  longitude: 47.2200198,
+  latitude: -1.5626764
 )
 poubelle_menagere5 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/bin10.jpg')),
   kind: 'menager',
   name: 'marcel',
-  longitude: 47.2134354,
-  latitude: -1.5556044
-)
-
-
-=======
-puts 'clean up DB'
-=======
-puts "clean up DB"
->>>>>>> master
-
-Bin.destroy_all
-User.destroy_all
-
-puts "Start database creation... "
-
-  poubelle_papier1 = Bin.create!(
-  photo: File.open(Rails.root.join('db/fixtures/bins/bin1.jpg')),
-  kind: 'papier',
-  name: 'mat',
-  longitude: 47.2185254,
-  latitude: -1.5671195
-)
-
-  poubelle_papier2 = Bin.create!(
-  photo: File.open(Rails.root.join('db/fixtures/bins/bin2.jpg')),
-  kind: 'papier',
-  name: 'jor',
-  longitude: 47.2187063,
-  latitude: -1.5685133
-)
-  poubelle_papier3 = Bin.create!(
-  photo: File.open(Rails.root.join('db/fixtures/bins/bin3.jpg')),
-  kind: 'papier',
-  name: 'sim',
-  longitude: 47.2186982,
-  latitude: -1.5689721
-)
-  poubelle_papier4 = Bin.create!(
-  photo: File.open(Rails.root.join('db/fixtures/bins/bin4.jpg')),
-  kind: 'papier',
-  name: 'jim',
-  longitude: 47.218705,
-  latitude: -1.5690397
-)
-  poubelle_papier5 = Bin.create!(
-  photo: File.open(Rails.root.join('db/fixtures/bins/bin5.jpg')),
-  kind: 'papier',
-  name: 'jem',
-  longitude: 47.2183323,
-  latitude: -1.5620625
-)
-  poubelle_menagere1 = Bin.create!(
-  photo: File.open(Rails.root.join('db/fixtures/bins/bin6.jpg')),
-  kind: 'menager',
-  name: 'john',
-  longitude: 47.2185081,
-  latitude: -1.5545757
-)
-poubelle_menagere2 = Bin.create!(
-  photo: File.open(Rails.root.join('db/fixtures/bins/bin7.jpg')),
-  kind: 'menager',
-  name: 'jacques',
-  longitude: 47.2140721,
-  latitude: -1.5576681
-)
-poubelle_menagere3 = Bin.create!(
-  photo: File.open(Rails.root.join('db/fixtures/bins/bin8.jpg')),
-  kind: 'menager',
-  name: 'josh',
-  longitude: 47.2124104,
-  latitude: -1.5556372
-)
-poubelle_menagere4 = Bin.create!(
-  photo: File.open(Rails.root.join('db/fixtures/bins/bin9.jpg')),
-  kind: 'menager',
-  name: 'bruno',
-  longitude: 47.2124234,
-  latitude: -1.5556044
-)
-poubelle_menagere5 = Bin.create!(
-  photo: File.open(Rails.root.join('db/fixtures/bins/bin10.jpg')),
-  kind: 'menager',
-  name: 'marcel',
-  longitude: 47.2134354,
-  latitude: -1.5556044
+  address: '5 Place Maréchal Foch, 44000 Nantes',
+  longitude: 47.2191746,
+  latitude: -1.5503382
 )
 
 victorine = User.create!(
   user_name: 'Victo',
   first_name: 'Victorine',
   last_name: 'Bramard',
-  address: '12 rue Emile Pehant',
+  address: '12 rue Emile Pehant, 44000 Nantes',
   email: 'victo@gmail.com',
   password: 'password',
   photo: File.open(Rails.root.join('#'))
@@ -185,7 +108,7 @@ glassbin1 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/container2.jpg')),
   kind: 'Verre',
   name: Faker::Games::Pokemon.name,
-  address: 'Chaussée de la Madeleine, 44000 Nantes'
+  address: 'Chaussée de la Madeleine, 44000 Nantes',
   lattitude: 47.212563,
   longitude: -1.553234,
 )
@@ -193,7 +116,7 @@ glassbin2 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/container3.jpg')),
   kind: 'Verre',
   name: Faker::Games::Pokemon.name,
-  address: '10 Quai Turenne, 44000 Nantes'
+  address: '10 Quai Turenne, 44000 Nantes',
   lattitude: 47.212585,
   longitude: -1.555094,
 )
@@ -201,7 +124,7 @@ glassbin3 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/container4.jpg')),
   kind: 'Verre',
   name: Faker::Games::Pokemon.name,
-  address: '128 Allée Brancas, 44000 Nantes'
+  address: '128 Allée Brancas, 44000 Nantes',
   lattitude: 47.213811,
   longitude: -1.556244,
 )
@@ -209,7 +132,7 @@ glassbin4 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/container5.jpg')),
   kind: 'Verre',
   name: Faker::Games::Pokemon.name,
-  address: '6 Allée de la Maison Rouge, 44000 Nantes'
+  address: '6 Allée de la Maison Rouge, 44000 Nantes',
   lattitude: 47.212944,
   longitude: -1.550890,
 )
@@ -222,22 +145,19 @@ glassbin5 = Bin.create!(
   longitude: -1.556855,
 )
 
-# Poubelles de plastique
-puts 'WIP creation of plastic bins...'
-
 plasticbin1 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/container_plastique1.jpg')),
   kind: 'Plastique',
   name: Faker::Games::Pokemon.name,
-  address: 'Allée Jean Bart, 44000 Nantes'
+  address: 'Allée Jean Bart, 44000 Nantes',
   lattitude: 47.214831,
-  longitude: -1.555654,
+  longitude: -1.555654
 )
 plasticbin2 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/container_plastique2.jpg')),
   kind: 'Plastique',
   name: Faker::Games::Pokemon.name,
-  address: 'Place du Commerce, 44000 Nantes'
+  address: 'Place du Commerce, 44000 Nantes',
   lattitude: 47.213293,
   longitude: -1.558250,
 )
@@ -245,7 +165,7 @@ plasticbin3 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/container_plastique3.jpg')),
   kind: 'Plastique',
   name: Faker::Games::Pokemon.name,
-  address: 'Bouffay, 44000 Nantes'
+  address: 'Bouffay, 44000 Nantes',
   lattitude: 47.214569,
   longitude: -1.553454,
 )
@@ -253,7 +173,7 @@ plasticbin4 = Bin.create!(
   photo: File.open(Rails.root.join('db/fixtures/bins/container_plastique4.jpg')),
   kind: 'Plastique',
   name: Faker::Games::Pokemon.name,
-  address: 'Rue Emile Pehant, 44000 Nantes'
+  address: 'Rue Emile Pehant, 44000 Nantes',
   lattitude: 47.212289,
   longitude: -1.548780,
 )
@@ -266,8 +186,5 @@ plasticbin5 = Bin.create!(
   longitude: -1.550971,
 )
 puts 'Fake Database has been created !'
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> master
+
 
