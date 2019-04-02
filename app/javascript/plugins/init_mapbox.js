5a3ee08a01f9902420228622671acad1e45dc066
Simon Keryhuel <keryhuel.simon@gmail.com>
2019-04-02 15:41:13 +0200

    pas footer at all, AJOUT DES PICTOS SUR LA MAP

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,8 +21,28 @@ const initMapbox = () => {
 
     const markers = JSON.parse(mapElement.dataset.markers);
 
+    // markers.forEach((marker) => {
+
+
+    //   // Pass the element as an argument to the new marker
+    //   new mapboxgl.Marker(element)
+    //     .setLngLat([marker.lng, marker.lat])
+    //     .setPopup(popup)
+    //     .addTo(map);
+    // });
+
     markers.forEach((marker) => {
-      new mapboxgl.Marker()
+
+      // Create a HTML element for your custom marker
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '35px';
+      element.style.height = '35px';
+
+
+      new mapboxgl.Marker(element)
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
     });
