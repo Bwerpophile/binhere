ac200df91c58111ca748336f87f87187cb175361
Joris Lespagnol <lespagnol.joris@gmail.com>
2019-04-03 16:59:23 +0200

    ajout de min-width/height sur avatar

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,7 +1,7 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   def default_url(*args)
-    ActionController::Base.helpers.asset_path("/images/default_placeholder.jpg")
+    ActionController::Base.helpers.asset_path("default_placeholder.jpg")
   end
   # Remove everything else
 end
