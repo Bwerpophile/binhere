73e08f705b5adcfc819e3fa62e3d82604cf89ee6
Jimmy Le Rouzic <jimmy.lerouzic@gmail.com>
2019-04-04 18:17:44 +0200

    deploy

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,7 +9,7 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom:15, duration: 0 });
+    map.fitBounds(bounds, { padding: 70, maxZoom:14, duration: 0 });
   };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
@@ -17,8 +17,7 @@ const initMapbox = () => {
 
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10',
-
+      style: 'mapbox://styles/mapbox/streets-v10'
     });
 
     let directions = new MapboxDirections({
